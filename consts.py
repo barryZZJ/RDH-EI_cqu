6d864e2379909a1a3d0ff855f0b4be8a0bbe0859
barryZZJ <850462618@qq.com>
2021-12-21 01:07:59 +0800

    添加默认测试图片

--- consts.py
+++ consts.py
@@ -1,4 +1,6 @@
 # 各种常量，方便写GUI和Main时直接使用
 AES_CONFIG_PATH = "aesConf.json"  # 加密相关配置文件默认存储路径
 EMBED_CONFIG_PATH = "embedConf.json"  # 信息嵌入相关配置文件默认存储路径
-PIC_TEST_PATH = "pics/lena.bmp"  # 测试用图片路径
+PIC_512_PATH = "pics/lena512.bmp"  # 测试用图片路径, 512x512
+PIC_8_PATH = "pics/lena8.bmp"  # 测试用图片路径, 8x8
+PIC_8_2_PATH = "pics/lena8_2.bmp"  # 测试用图片路径, 8x8
