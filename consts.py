f93a169d9a14b188289f905ff0bbcad92512742b
barryZZJ <850462618@qq.com>
2021-12-21 22:20:06 +0800

    更新信息嵌入相关框架

--- consts.py
+++ consts.py
@@ -4,3 +4,11 @@ EMBED_CONFIG_PATH = "embedConf.json"  # 信息嵌入相关配置文件默认存�
 PIC_512_PATH = "pics/lena512.bmp"  # 测试用图片路径, 512x512
 PIC_8_PATH = "pics/lena8.bmp"  # 测试用图片路径, 8x8
 PIC_8_2_PATH = "pics/lena8_2.bmp"  # 测试用图片路径, 8x8
+
+
+PIC_16_PATH = "pics/lena16.bmp"  # 嵌入信息测试用图片，16x16
+EMBED_KEY_LEN = 8  # 嵌入密钥长度
+EMBED_KEY_DEBUG = 132  # 默认密钥
+EMBED_PARAM_W = 1
+EMBED_PARAM_U = 2
+EMBED_DATA_DEBUG = b'testtesttesttest'
