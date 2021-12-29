b56e5413b1e2d808013b505e06b3bcc89c15625b
xiaoqiang <2425161282@qq.com>
2021-12-29 21:41:46 +0800

    完成加密解密

--- aesutil.py
+++ aesutil.py
@@ -19,15 +19,12 @@ class AESUtil:
             self.iv, self.key = self.read_config(config)
         else:
             self.iv, self.key = self.rand_init()
-        self.save_config()
-        print(self.iv,self.key)
     def save_config(self, config: str = AES_CONFIG_PATH):
         """
         保存iv和key到配置文件中，可以考虑构建字典然后JSON.dump，则配置文件后缀名为.json
         :param config: 配置文件路径字符串
         """
-        with open(config, "r", encoding="utf-8") as f:
-            data = json.load(f)
+        data = {}
         data['iv'] = self.iv
         data['key'] = self.key
         with open(config, "w") as f:
@@ -37,9 +34,9 @@ class AESUtil:
         从配置文件中读取iv和key，可以考虑用JSON.load读取字典
         :param config: 配置文件路径字符串
         """
-        with open(config) as f:
+        with open(config,"r") as f:
             data = json.load(f)
-        return [data['iv'], data['key']]
+        return data['iv'], data['key']
     def rand_init(self) :
         """随机初始化iv和key"""
         str = ''
@@ -49,10 +46,9 @@ class AESUtil:
 
     def encrypt(self, m: bytes) -> bytes:
         """对明文字节流m加密，返回密文字节流"""
-        with open(AES_CONFIG_PATH) as f:
-            data = json.load(f)
-        key=(data['key']).encode()
-        iv=(data['iv']).encode()
+
+        key=(self.key).encode()
+        iv=(self.iv).encode()
         cipher = AES.new(key, AES.MODE_CBC, iv)
         msg=cipher.encrypt(m)
         return msg
@@ -62,9 +58,9 @@ class AESUtil:
         对密文字节流c解密，返回明文字节流
         其他模块解密需要动态修改iv，因此提取为参数，默认使用self.iv即可
         """
-        with open(AES_CONFIG_PATH) as f:
-            data = json.load(f)
-        key=(data['key']).encode()
+        if not iv:
+            iv = self.iv
+        key=(self.key).encode()
         cipher = AES.new(key, AES.MODE_CBC, iv)
         msg = cipher.decrypt(c)
         return msg
