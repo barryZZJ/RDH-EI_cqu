7b08c5451d23178989f5fdbcbb902c8e5edd6a01
barryZZJ <850462618@qq.com>
2021-12-30 15:43:11 +0800

    修改密钥默认配置文件名

--- consts.py
+++ consts.py
@@ -1,5 +1,5 @@
 # 各种常量，方便写GUI和Main时直接使用
-AES_CONFIG_PATH = "aesConf.json"  # 加密相关配置文件默认存储路径
+AES_CONFIG_PATH = "aesConf.key"  # 加密相关配置文件默认存储路径
 EMBED_CONFIG_PATH = "embedConf.json"  # 信息嵌入相关配置文件默认存储路径
 PIC_512_PATH = "pics/lena512.bmp"  # 测试用图片路径, 512x512
 PIC_128_PATH = "pics/lena128.bmp"  # 测试用图片路径, 128x128
