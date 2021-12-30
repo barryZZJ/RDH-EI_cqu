f5b4d4c396e71db57691847736340340ce5868b3
barryZZJ <850462618@qq.com>
2021-12-30 15:31:19 +0800

    添加128x128图片方便测试

--- consts.py
+++ consts.py
@@ -2,6 +2,7 @@
 AES_CONFIG_PATH = "aesConf.json"  # 加密相关配置文件默认存储路径
 EMBED_CONFIG_PATH = "embedConf.json"  # 信息嵌入相关配置文件默认存储路径
 PIC_512_PATH = "pics/lena512.bmp"  # 测试用图片路径, 512x512
+PIC_128_PATH = "pics/lena128.bmp"  # 测试用图片路径, 128x128
 PIC_8_PATH = "pics/lena8.bmp"  # 测试用图片路径, 8x8
 PIC_8_2_PATH = "pics/lena8_2.bmp"  # 测试用图片路径, 8x8
 
