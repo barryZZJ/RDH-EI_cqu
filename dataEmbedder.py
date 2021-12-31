df55481a61b447f043e41f7bfef99d9df3d40734
Zejun Zhou <48206733+barryZZJ@users.noreply.github.com>
2021-12-31 17:09:58 +0800

    实现无损解密 (#32)
    * 完成perfect_decrypt内容
    
    实现optimize
    
    * 实现optimize
    
    * 添加todo
    
    * 修改init接口参数类型；部分类函数改为内部方法、静态方法；添加load_config()

--- dataEmbedder.py
+++ dataEmbedder.py
@@ -1,16 +1,18 @@
+
+import numpy as np
+from numpy.typing import NDArray
+import random
 import secrets
 from bitstring import BitStream
 from typing import List
 from PIL import Image
+from Crypto.Cipher import AES
+
 from consts import *
 from aesutil import AESUtil
-from imgUil import segment_every, join
-from imgUil import img_to_bitstream
-import numpy as np
-from numpy.typing import NDArray
-import random
 
-from Crypto.Cipher import AES
+from imgUil import segment_every, join, bitstream_to_blocks, _from_blocks
+#TODO 添加可嵌入量接口
 
 DEBUG = True
 
@@ -34,9 +36,9 @@ class DataEmbedder:
             self.key = EMBED_KEY_DEBUG
         else:
             if config:
-                self.key = self.read_config(config)
+                self.key = self._read_config(config)
             else:
-                self.key = self.rand_init()
+                self.key = self._rand_init()
 
         if aesconfig:
             self.aes = AESUtil(aesconfig)
@@ -53,7 +55,9 @@ class DataEmbedder:
         with open(config, 'wb') as f:
             f.write(self.key)
 
-    def read_config(self, config: str = EMBED_CONFIG_PATH) -> bytes:
+
+    @staticmethod
+    def _read_config(config: str = EMBED_CONFIG_PATH) -> bytes:
         """
         从配置文件中读取key，并返回key
         *yzy*
@@ -64,7 +68,11 @@ class DataEmbedder:
             key = f.read()
         return key
 
-    def rand_init(self) -> bytes:
+    def load_config(self, config: str = EMBED_CONFIG_PATH):
+        self.key = self._read_config(config)
+
+    @staticmethod
+    def _rand_init() -> bytes:
         """
         随机初始化key，并返回key。
         *yzy*
@@ -107,18 +115,48 @@ class DataEmbedder:
         """
         pass
 
-    def perfect_decrypt(self):
+    def perfect_decrypt(self, data: bytes, img: bytes) -> Image.Image:
         """
         如果同时拥有加密密钥和嵌入密钥，可以无损解密原始图片。
         需要调用AESUtil相关功能。
         我认为应该由信息嵌入器进行处理，因为该功能只有信息嵌入者知道。
 
-        如果可以先提取再解密实现的话，就不需要这个函数
-
         *zzj*
         """
-        if not self.aes: return
-        pass
+        assert isinstance(self.aes, AESUtil)
+        img = BitStream(bytes=img)
+        blocks_of_LSB = self.extract_LSB(img)
+        blocks_of_LSB = self.reverse_shuffle(blocks_of_LSB)
+        bitstream_LSB = join(blocks_of_LSB)
+        K = len(blocks_of_LSB)
+        L = K//EMBED_PARAM_U
+        # 把bitstream_LSB分成L个组，每组包含了u个块的LSB，每组为64*u bit
+        g = segment_every(bitstream_LSB, 64*EMBED_PARAM_U)
+        H = self.gen_matrix()
+        data = self.encrypt_data(data)
+        data = BitStream(bytes=data)
+        datas = segment_every(data, EMBED_PARAM_W)
+        # 以组为单位，尝试恢复 gk
+        # 恢复原理：遍历 gk[:w]所有可能的取值，全部解密出原图（共w位，故2^w个图），通过数字统计关系选出最合理的那一个
+        best_blocks = []
+        e_grouped = segment_every(img, 512*EMBED_PARAM_U)  # u个块(64b*8 * u)一组，共L组
+        for k in range(0, L-1 + 1):
+            blocks_k = [None] * (2**EMBED_PARAM_W)  # type: List[List[Image.Image]] # shape: (2^w, u)
+            # blocks_k[l] = 第k组中，第l种情况的u个块
+            e_k = e_grouped[k].copy()  # FIXME 能否删除copy
+            for l in range(2**EMBED_PARAM_W):
+                t_l = BitStream(uint=l, length=EMBED_PARAM_W)
+                r_k = datas[k] ^ t_l
+                w_k = g[k] ^ self.dot(r_k, H)  # TODO 点乘待实现
+                e_k = self.sub_group_LSB(e_k, w_k)  # 把e_k的LSB替换为w_k，即论文中的r
+                e_k = self.aes.decrypt(e_k.bytes, iv=e_grouped[k-1][-128:].bytes if k > 0 else None)  # TODO 正确性待检查 # 只解密第k组的u个块，iv为上一组的最后128bit
+                e_k = BitStream(bytes=e_k)
+                P_kl = bitstream_to_blocks(e_k)  # u个8x8的像素块列表
+                blocks_k[l] = P_kl  # 记录第l种情况下的u个块
+            best_l = self.optimize(blocks_k)  # TODO 正确性待检查
+            best_blocks.extend(blocks_k[best_l])  # 最合理的u个块添加到结果中
+        best_img = _from_blocks(best_blocks)
+        return best_img
 
     def extract_lsb(self, img: BitStream) -> List[BitStream]:
         """
@@ -200,6 +238,13 @@ class DataEmbedder:
         res = np.dot(bits, matrix)
         return BitStream(auto=res) # int列表自动识别为二进制流
 
+    #TODO
+    def dot(self, bits: BitStream, matrix) -> BitStream:
+        """
+        比特流与矩阵点乘
+        """
+        pass
+
     def encrypt_data(self, data: bytes) -> bytes:
         """
         对要嵌入的数据用self.key加密，且输入长度与输出长度相同。
@@ -243,12 +288,39 @@ class DataEmbedder:
         """
         pass
 
-    def optimize(self, blocksk: List[List[Image.Image]]) -> int:
+    def optimize(self, blocks_k: List[List[Image.Image]]) -> int:
         """
-        优化算法算出最合理的图片对应的情况。返回对应情况的下标。
+        优化算法算出最合理的图片对应的情况。返回该情况对应的下标。
         *zzj*
+
+        :param blocks_k: shape: (2**w, u)，2**w种情况下的u个块
         """
-        pass
+        def blocks_group_diff(blocks: np.ndarray) -> int:
+            """
+            遍历一组u个块，每一块的 sum(每一行与上一行像素值之差 + 每一列与上一列像素值之差) 之和。
+            等价于
+            u个块看作一整张8 x 8u的图片，计算sum(每一行与上一行像素值之差 + 每一列与上一列像素值之差)
+
+            :param blocks: 一组u个块，shape: (8, 8*u)
+            """
+            def row_diff(blocks: np.ndarray):
+                return np.sum(np.abs(blocks[1:] - blocks[:-1]))
+            def col_diff(blocks: np.ndarray):
+                return np.sum(np.abs(blocks[:,1:] - blocks[:,:-1]))
+            rowDiff = row_diff(blocks)
+            colDiff = col_diff(blocks)
+            return rowDiff + colDiff
+
+        # 把 List[List[Image.Image]] 转换为 ndarray, shape: (2**w, 8, 8*u)
+        npblocks_k = [np.concatenate(blocks, axis=1) for blocks in blocks_k]  # 每个块转换为(8, 8*u) 的ndarray
+        npblocks_k = [np.expand_dims(blocks, axis=0) for blocks in npblocks_k]  # 每个块增加一维，变为(1, 8, 8*u)
+        npblocks_k = np.concatenate(npblocks_k, axis=0)  # shape: (2**w, 8, 8*u) 2**w个块叠加在一起
+        # 计算所有2**w种情况下每组的blocks_group_diff
+        block_diffs = np.asarray([blocks_group_diff(blocks) for blocks in npblocks_k])  # shape: (2**w)
+        # 求最小的那一个的下标
+        min_ind = np.argmin(block_diffs)
+        return min_ind
+
 
 if __name__ == '__main__':
     #TODO
@@ -262,3 +334,4 @@ if __name__ == '__main__':
     img = img_to_bitstream(PIC_128_PATH)
     data = EMBED_DATA_DEBUG  # 128bit, u=2, w=1 时适合嵌入128x128图片
     nimg = de.embed(data, img.bytes)
+
