9bb528b5a6a6d9d1d554592c9cac2ce8bb06c140
barryZZJ <850462618@qq.com>
2022-01-02 21:54:05 +0800

    清除不用的常量

--- consts.py
+++ consts.py
@@ -8,10 +8,7 @@ PIC_8_2_PATH = "pics/lena8_2.bmp"  # 测试用图片路径, 8x8
 
 
 PIC_16_PATH = "pics/lena16.bmp"  # 嵌入信息测试用图片，16x16
-# TODO 下面这些最后放到dataEmbedder.py里
 EMBED_KEY_LEN = 16  # 嵌入密钥长度，字节数
 EMBED_KEY_DEBUG = bytes.fromhex('a9ed88811c8a7864283fde603abd5132')  # 默认密钥。16字节 = 128比特。考虑到数据加密使用AES，密钥也应该是字节。
 EMBED_PIC_DEBUG = PIC_128_PATH
-EMBED_PARAM_W = 1
-EMBED_PARAM_U = 2
 EMBED_DATA_DEBUG = b'testtesttesttest'
