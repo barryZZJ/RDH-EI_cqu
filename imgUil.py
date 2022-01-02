10afbff680f49a632f03927d6f6266c70f5b7873
Zejun Zhou <48206733+barryZZJ@users.noreply.github.com>
2022-01-02 22:07:28 +0800

    大改整体框架，主要完成数据嵌入读取模块 (#34)
    * 实现嵌入信息 (#31)
    
    * 提取lsb函数：extract_LSB
    打乱lsb函数：shuffle
    还原lsb函数：reverse_shuffle
    
    * 导入random包 修改embed函数的img参数bytes->List[BitStream] 完成embed函数 完成gen—matrix函数 修改sub_LSB函数的sub参数BitStream->List[BitStream] 完成sub_LSB函数
    
    * 提取lsb函数：extract_LSB
    打乱lsb函数：shuffle
    还原lsb函数：reverse_shuffle
    
    * 实现嵌入信息
    
    * 提取lsb函数：extract_LSB
    打乱lsb函数：shuffle
    还原lsb函数：reverse_shuffle
    
    * 提取lsb函数：extract_LSB
    打乱lsb函数：shuffle
    还原lsb函数：reverse_shuffle
    
    * 修改结构，与zzj的decrypt_perfect函数中的变量命名与结构一致
    
    Co-authored-by: yzyyzyyzy <44904490+yzyyzyyzy@users.noreply.github.com>
    Co-authored-by: MagicZT <81006183+MagicZT@users.noreply.github.com>
    
    * 实现无损解密 (#32)
    
    * 完成perfect_decrypt内容
    
    实现optimize
    
    * 实现optimize
    
    * 添加todo
    
    * 修改init接口参数类型；部分类函数改为内部方法、静态方法；添加load_config()
    
    * 修改默认密钥配置文件名
    
    * 使用secrets库生成安全性更强的随机密钥
    
    * 部分接口修改为内部函数+静态函数
    
    * _sub_group_lsb
    
    * 提取信息
    
    * 一个方法改为内部方法
    
    * 重构，把嵌入信息和提取信息分为两个类。同时实现根据数据量大小和密文流自动确定合适的参数
    
    * 修改密钥生成方式，便于读取配置文件
    
    * 修改密钥、数据填充方式，便于读取配置文件及嵌入处理
    
    修改密钥、数据填充方式，便于读取配置文件及嵌入处理
    
    * 调整框架细节，删除shuffle，考虑分组时K无法整除U的情况
    
    * 修正无损解密算法
    
    * 修正图片生成
    
    * 标注论文有错误的情况
    
    * 清除不用的常量
    
    * 忽略掉密钥及生成文件
    
    * 添加主入口
    
    Co-authored-by: yzyyzyyzy <44904490+yzyyzyyzy@users.noreply.github.com>
    Co-authored-by: MagicZT <81006183+MagicZT@users.noreply.github.com>

--- imgUil.py
+++ imgUil.py
@@ -67,7 +67,7 @@ def segment_every(bitstream: BitStream, bits: int) -> List[BitStream]:
     注：加密器使用该输出时，可以通过列表里每个BitStream对象的bytes成员(如bitstream.bytes)把BitStream对象转换为字节。
     """
     list_of_bitplanes = []
-    for k in range(0,int(len(bitstream)/bits)):
+    for k in range(0, len(bitstream)//bits):
         list_of_bitplanes.append(bitstream[k*bits:(k+1)*bits])
     return list_of_bitplanes
 
@@ -81,12 +81,12 @@ def join(seg: List[BitStream]) -> BitStream:
 
 def bitstream_to_img(bitstream: BitStream) -> Image.Image:
     """输入密文字节流e，计算对应像素值，解码为图片，即img_to_bitstream的逆过程。"""
-    blocks = bitstream_to_blocks(bitstream)
+    blocks = _bitstream_to_blocks(bitstream)
     img = _from_blocks(blocks)
     #! 如果有需要可以添加保存到本地的函数
     return img
 
-def bitstream_to_blocks(bitstream: BitStream) -> List[Image.Image]:
+def _bitstream_to_blocks(bitstream: BitStream) -> List[Image.Image]:
     """输入密文字节流e，计算对应像素值，解码为各个图片块，不需要拼接"""
     list_of_bitplanes = segment_every(bitstream, 512)  # 分成K个512 bits的列表，每个元素是一个块的位平面列表
     blocks = []
@@ -103,7 +103,7 @@ def _bitplanes_to_block(bitplanes: BitStream) -> Image.Image:
         for y in range(0,8):
             bitplanes2 += BitStream(bool=bitplanes[x+64*(7-y)])
     list_of_pixels = segment_every(bitplanes2, 8)
-    img = np.asarray([pixel.uint for pixel in list_of_pixels])
+    img = np.asarray([pixel.uint for pixel in list_of_pixels], dtype=np.uint8)
     img = img.reshape(8, 8)
     img = Image.fromarray(img)
     return img
