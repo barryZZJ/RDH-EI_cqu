83bb1ad5f299abef6f860ebd4933281ac8cc8903
Zejun Zhou <48206733+barryZZJ@users.noreply.github.com>
2022-01-04 22:03:07 +0800

    Revert "hjk"

--- gui/downloader_main.py
+++ gui/downloader_main.py
@@ -11,60 +11,43 @@ from PyQt5 import QtWidgets
 from bitstring import BitStream
 
 
-class extract_worker(QObject):
-    extract_finish_signal = pyqtSignal(str)
+class extract_thread(QObject):
+    extract_finish_signal = pyqtSignal()
 
     def __init__(self, main_instance):
-        super(extract_worker, self).__init__()
+        super(extract_thread, self).__init__()
         self.main_instance = main_instance
-        self.img_bits = None
-        self.embed_data = None
 
     def do_extract(self):
         try:
             pic_bitstream = img_to_bitstream(self.main_instance.path_load_pic)
-            self.img_bits = pic_bitstream
-            self.embed_data = self.main_instance.data_extractor.extract(img=pic_bitstream.bytes)
-            self.extract_finish_signal.emit('Y')
-        except IOError:
-            QMessageBox.critical(None, "错误", "路径错误", QMessageBox.Yes | QMessageBox.No)
-            self.extract_finish_signal.emit('N')
+            self.main_instance.img_bits = pic_bitstream
+            self.main_instance.embed_data = self.main_instance.data_extractor.extract(img=pic_bitstream.bytes)
         except Exception as e:
             QMessageBox.critical(None, "错误", str(e), QMessageBox.Yes | QMessageBox.No)
-            self.extract_finish_signal.emit('N')
-
-    def get_data(self):
-        return [self.img_bits, self.embed_data]
+        finally:
+            self.extract_finish_signal.emit()
 
 
-class decrypt_worker(QObject):
-    decrypt_finish_signal = pyqtSignal(str)
+class decrypt_thread(QObject):
+    decrypt_finish_signal = pyqtSignal()
 
     def __init__(self, main_instance):
-        super(decrypt_worker, self).__init__()
+        super(decrypt_thread, self).__init__()
         self.main_instance = main_instance
 
     def do_decrypt(self):
         try:
-            if self.main_instance.img_bits is None:
-                pic_bytes = img_to_bitstream(self.main_instance.path_load_pic).bytes
-            else:
-                pic_bytes = self.main_instance.img_bits.bytes
-            if self.main_instance.perfect_flag:
-                perfect_img = self.main_instance.data_extractor.perfect_decrypt(
-                    data=self.main_instance.embed_data, img=pic_bytes)
-                perfect_img.save(self.main_instance.path_save_pic)
-            else:
-                decrypt_img_bits = self.main_instance.aes_util.decrypt(pic_bytes)
-                decrypt_img = bitstream_to_img(BitStream(bytes=decrypt_img_bits))
-                decrypt_img.save(self.main_instance.path_save_pic)
-            self.decrypt_finish_signal.emit('Y')
+            perfect_bits = self.main_instance.data_extractor.perfect_decrypt(
+                data=self.main_instance.embed_data, img=self.main_instance.img_bits)
+            perfect_img = bitstream_to_img(BitStream(bytes=perfect_bits))
+            perfect_img.save(self.main_instance.path_save_pic)
         except IOError:
             QMessageBox.critical(None, "错误", "路径错误", QMessageBox.Yes | QMessageBox.No)
-            self.decrypt_finish_signal.emit('N')
         except Exception as e:
             QMessageBox.critical(None, "错误", str(e), QMessageBox.Yes | QMessageBox.No)
-            self.decrypt_finish_signal.emit('N')
+        finally:
+            self.decrypt_finish_signal.emit()
 
 
 class downloader_main(QtWidgets.QDialog, downloader.Ui_Form, QThread):
@@ -91,18 +74,18 @@ class downloader_main(QtWidgets.QDialog, downloader.Ui_Form, QThread):
         # 线程定义
         self.decrypt_qthread = QThread()
         self.extract_qthread = QThread()
-        self.extract_worker = extract_worker(self)
-        self.decrypt_worker = decrypt_worker(self)
-        self.extract_worker.moveToThread(self.extract_qthread)
-        self.decrypt_worker.moveToThread(self.decrypt_qthread)
+        self.extract_thread = extract_thread(self)
+        self.decrypt_thread = decrypt_thread(self)
+        self.extract_thread.moveToThread(self.extract_qthread)
+        self.decrypt_thread.moveToThread(self.decrypt_qthread)
 
         # 启动线程
-        self.extract_qthread.started.connect(self.extract_worker.do_extract)
-        self.decrypt_qthread.started.connect(self.decrypt_worker.do_decrypt)
+        self.extract_qthread.started.connect(self.extract_thread.do_extract)
+        self.decrypt_qthread.started.connect(self.decrypt_thread.do_decrypt)
 
         # 结束线程
-        self.extract_worker.extract_finish_signal.connect(self.extract_thread_finish)
-        self.decrypt_worker.decrypt_finish_signal.connect(self.decrypt_thread_finish)
+        self.extract_thread.extract_finish_signal.connect(self.extract_thread_quit)
+        self.decrypt_thread.decrypt_finish_signal.connect(self.decrypt_thread_quit)
         self.extract_qthread.finished.connect(self.extract_thread_quit)
         self.decrypt_qthread.finished.connect(self.decrypt_thread_quit)
 
@@ -129,8 +112,6 @@ class downloader_main(QtWidgets.QDialog, downloader.Ui_Form, QThread):
         else:
             # 将选中路径同步到LineEdit上
             self.lineEdit_load_decrypt_key.setText(file_name)
-            self.path_load_decrypt_key = file_name
-            self.perfect_flag = False
             return
 
     # 设定加载嵌入密钥路径
@@ -145,8 +126,6 @@ class downloader_main(QtWidgets.QDialog, downloader.Ui_Form, QThread):
         else:
             # 将选中路径同步到LineEdit上
             self.lineEdit_load_embed_key.setText(file_name)
-            self.path_load_embed_key = file_name
-            self.perfect_flag = False
             return
 
     # 设定加载图片路径
@@ -164,22 +143,23 @@ class downloader_main(QtWidgets.QDialog, downloader.Ui_Form, QThread):
         else:
             # 将选中路径同步到LineEdit上
             self.lineEdit_load_pic.setText(file_name)
-            self.path_load_pic = file_name
             self.embed_data = None
-            self.perfect_flag = False
             return
 
     # 获取嵌入信息
     def get_embed_data(self):
-        self.path_load_pic = self.lineEdit_load_pic.text()
-        self.path_load_embed_key = self.lineEdit_load_embed_key.text()
         self.data_extractor.load_config(config=self.path_load_embed_key)
-        self.pushButton_preview_embed_data.setEnabled(False)
         self.extract_qthread.start()
 
     # 预览嵌入信息
     def preview_embed_data(self):
         self.get_embed_data()
+        if self.embed_data is not None:
+            self.child_window = Child()
+            self.child_window.show()
+            self.child_window.preview(str(self.embed_data))
+        else:
+            QMessageBox.critical(None, "错误", "没有获得嵌入信息！", QMessageBox.Yes | QMessageBox.No)
         return
 
     # 保存嵌入信息
@@ -225,37 +205,16 @@ class downloader_main(QtWidgets.QDialog, downloader.Ui_Form, QThread):
             else:
                 QMessageBox.critical(None, "错误", "无密钥", QMessageBox.Yes | QMessageBox.No)
             # 启动解密保存线程
-            self.pushButton_decrypt_save_pic.setEnabled(False)
             self.decrypt_qthread.start()
             return
 
-    # 线程结束
-    def extract_thread_finish(self, info):
-        if info == 'Y':
-            self.img_bits, self.embed_data = self.extract_worker.get_data()
-            self.child_window = Child()
-            self.child_window.show()
-            self.child_window.preview(self.embed_data.decode('utf8'))
-        else:
-            pass
-        self.pushButton_preview_embed_data.setEnabled(True)
-        self.extract_thread_quit()
-
-    def decrypt_thread_finish(self, info):
-        if info == 'Y':
-            QMessageBox.information(self, "提示", "保存成功", QMessageBox.Yes)
-            pass
-        else:
-            QMessageBox.critical(self, "错误", "保存失败", QMessageBox.Yes)
-        self.pushButton_decrypt_save_pic.setEnabled(True)
-        self.decrypt_thread_quit()
-
-    # 线程退出
+    # 线程结束退出
     def extract_thread_quit(self):
         self.extract_qthread.quit()
         return
 
     def decrypt_thread_quit(self):
+        QMessageBox.information(self, "提示", "保存成功", QMessageBox.Yes)
         self.decrypt_qthread.quit()
         return
 
